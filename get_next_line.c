146abd36ecaa9bb9be950c1656473fa82e6cb83c
oshiro17 <77593715+oshiro17@users.noreply.github.com>
2022-08-23 15:29:26 +0900

    Allow first argument NULL in ft_strrchar

--- get_next_line.c
+++ get_next_line.c
@@ -18,7 +18,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		length;
 
 	i = 0;
-	if (!s1 || !s2)
+	if (!s2)
 		return (NULL);
 	length = ft_strlen((char *)s1, '\0') + ft_strlen((char *)s2, '\0');
 	nstr = (char *)malloc(sizeof(char) * (length + 1));
@@ -93,8 +93,11 @@ char *get_next_line(int fd)
 	static char	*save;
 	ssize_t		len;
 
-	buff = malloc(BUFFER_SIZE);
-	while (!(ft_strrchr(save, '\n')) && len != 0)
+	len = 1;
+	buff = malloc(BUFFER_SIZE + 1);
+	if (!buff)
+		return(NULL);
+	while ((ft_strrchr(save, '\n')) == NULL && len != 0)
 	{
 		len = read(fd, buff, BUFFER_SIZE);
 		buff[len] = '\0';
@@ -106,19 +109,56 @@ char *get_next_line(int fd)
 	return(save);
 }
 
-#include "get_next_line.h"
-
-int	main(void)
+int main(void)
 {
-	printf("aiuek");
-	int		fd;
-	char	*line;
+	char *line;
+	int fd;
 
-	line = "";
-	fd = open("text.txt", O_RDONLY);
+	//fd = open("./gnl_instructions.txt", O_RDONLY);
+	//fd = open("./file.txt", O_RDONLY);
+	//fd = open("./num_even.txt", O_RDONLY);
+	fd = open("file.txt", O_RDONLY);
+	//fd = open("./41_with_nl", O_RDONLY);
+	//fd = open("./43_no_nl", O_RDONLY);
+	//line = ft_substr("", 0, 0);
+	//while (line)
+	//{
+	//	line = get_next_line(fd);
+	//	printf("line : %s", line); 
+	//	free (line);
+	//}
+	//line = get_next_line(1000);
+	//printf("line : %s", line); 
+	//free (line);
+	//line = get_next_line(-1);
+	//printf("line : %s", line); 
+	//free (line);
 	line = get_next_line(fd);
-	printf(" %s\n", line);
-	printf(" %s", line);
-	free(line);
-	return (0);
+	printf("line : %s", line); 
+	free (line);
+	line = get_next_line(fd);
+	printf("line : %s", line); 
+	free (line);
+	line = get_next_line(fd);
+	printf("line : %s", line); 
+	free (line);
+	line = get_next_line(fd);
+	printf("line : %s", line); 
+	free (line);
+	line = get_next_line(fd);
+	printf("line : %s", line); 
+	free (line);
+	line = get_next_line(fd);
+	printf("line : %s", line); 
+	free (line);
+	line = get_next_line(fd);
+	printf("line : %s", line); 
+	free (line);
+	line = get_next_line(fd);
+	printf("line : %s", line); 
+	free (line);
+	line = get_next_line(fd);
+	printf("line : %s", line); 
+	free (line);
+	return 0;
 }
