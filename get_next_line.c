48624bbd4780d7464984deae0f2109e8d732a6e5
oshiro17 <77593715+oshiro17@users.noreply.github.com>
2022-09-05 05:40:39 +0900

    kimotiee

--- get_next_line.c
+++ get_next_line.c
@@ -5,20 +5,12 @@ char	*ft_get_line(char *save)
 	int		i;
 	char	*to_line;
 
-	ft_strlen(save,'\n');
-	// printf("[test]\n");
-	// fflush(stdout);
-	ft_strlen(save,'\n');
 	if (!save[0])
 		return (NULL);
-	to_line = (char *)malloc(sizeof(char) * ((ft_strlen(save,'\n')) + 1));
-
-	// printf("[hi]\n");
-	// fflush(stdout);
+	to_line = (char *)malloc(sizeof(char) * ((ft_strlen(save, '\n')) + 1));
 	if (!to_line)
 		return (NULL);
 	i = 0;
-
 	while (save[i] && save[i] != '\n')
 	{
 		to_line[i] = save[i];
@@ -30,7 +22,6 @@ char	*ft_get_line(char *save)
 		i++;
 	}
 	to_line[i] = '\0';
-
 	return (to_line);
 }
 
@@ -48,7 +39,7 @@ char	*ft_save(char *save)
 		free(save);
 		return (NULL);
 	}
-	s = (char *)malloc(sizeof(char) * (ft_strlen(save,'\0') - i + 1));
+	s = (char *)malloc(sizeof(char) * (ft_strlen(save, '\0') - i + 1));
 	if (!s)
 		return (NULL);
 	i++;
@@ -59,62 +50,58 @@ char	*ft_save(char *save)
 	free(save);
 	return (s);
 }
-
-char *get_next_line(int fd)
+char	*buff_to_save(char *save, int fd)
 {
-	char	*buff;
-	char	*line;
-	static char	*save[256];
-	ssize_t		len;
+	ssize_t	len;
+	char		*buff;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || fd > 255)
-		return (0);
 	len = 1;
 	buff = malloc(BUFFER_SIZE + 1);
 	if (!buff)
 		return(NULL);
-
-	while (!(ft_strchr(save[fd], '\n')) && len > 0)
+	//'\n'がないときに入る
+	while (!(ft_strchr(save,'\n')) && len > 0)
 	{
 		len = read(fd, buff, BUFFER_SIZE);
 		if (len == -1)
-		{
-			free(buff);
-			return (NULL);
+		{	
+			free (buff);
+			return(NULL);
 		}
 		buff[len] = '\0';
-		save[fd] = ft_strjoin(save[fd], buff);
-		// printf("save: [%s], buff: [%s]\n", save[fd], buff);
-		// if(*buff == '\0')
-		// 	break;
-		fflush(stdout);
+		save = ft_strjoin(save, buff);
 	}
 	free(buff);
+	return(save);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*save[256];
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd > 255)
+		return (0);
+//	len = 1;
+	// buff = malloc(BUFFER_SIZE + 1);
+	// if (!buff)
+	// 	return (NULL);
+	// while (!(ft_strchr(save[fd], '\n')) && len > 0)
+	// {
+	// 	len = read(fd, buff, BUFFER_SIZE);
+	// 	if (len == -1)
+	// 	{
+	// 		free(buff);
+	// 		return (NULL);
+	// 	}
+	// 	buff[len] = '\0';
+	// 	save[fd] = ft_strjoin(save[fd], buff);
+	// }
+	// free(buff);
+	save[fd] = buff_to_save(save[fd], fd);
 	if (!save[fd])
 		return (NULL);
 	line = ft_get_line(save[fd]);
-	// printf("1save: [%s] line: [%s]\n", save, line);
-	// fflush(stdout);
 	save[fd] = ft_save(save[fd]);
-	// printf("2. save: [%s] line: [%s]\n", save, line);
-	return(line);
+	return (line);
 }
-
-// int	main(void)
-// {
-// 	int		fd;
-// 	char	*line;
-// 	line = "";
-// 	fd = open("file.txt", O_RDONLY);
-// 	printf("%d",fd);
-
-// 	while(line)
-// 	{
-// 		line = get_next_line(fd);
-// 		if (line)
-// 			printf("> %s", line);
-// 		free(line);
-// 	}
-// 	free(line);
-// 	return (0);
-// }
